0313dd29cb5d78a238d9c89568897d5ee3482b13
Administrator <Administrator@CANADA.CA>
2020-01-25 13:41:20 -0500

    first commit

---
+++ UseADSISearcherTypeAccelerator.ps1
@@ -0,0 +1,33 @@
+﻿$RootADConfigPartition = ([ADSI]'LDAP://RootDSE').configurationNamingContext
+
+Write-host "Rood AD config partition" -BackgroundColor Yellow
+$RootADConfigPartition
+
+Write-Host "Creating ADSISearcher:" -BackgroundColor Yellow
+Write-Host "LDAP://$RootADConfigPartition"
+
+$ADConnection = [ADSISearcher]""
+$ADConnection.SearchRoot = "LDAP://$RootADConfigPartition"
+
+$ADConnection.Filter = "(objectCategory=CN=MS-Exch-Exchange-Server,CN=Schema,$RootADConfigPartition)"
+
+write-host "Search root:" -BackgroundColor Yellow
+$ADConnection | select -ExpandProperty SearchRoot
+
+
+$allresults = $ADConnection.FindAll()
+
+$Coll = @()
+Foreach ($item in $allresults) {
+    $coll+=[pscustomobject]@{
+        Name = $Item.Properties['name']
+        #ObjectCategory = $item.Properties['objectcategory']
+        #ObjectClass = $item.Properties['objectClass']
+        #Version1 = $item.Properties['msExchVersion']
+        #VErsion2 = $item.Properties['msExchMinAdminVersion']
+        Version = $Item.Properties['serialNumber']
+        Roles = $Item.Properties['msExchCurrentServerRoles']
+
+    }
+}
+$coll | fl
