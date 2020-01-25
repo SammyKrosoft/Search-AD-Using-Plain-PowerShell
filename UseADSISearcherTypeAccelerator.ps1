5ebe9bdf2d86470da06100550206251a8df1c004
Administrator <Administrator@CANADA.CA>
2020-01-25 13:56:34 -0500

    update

--- UseADSISearcherTypeAccelerator.ps1
+++ UseADSISearcherTypeAccelerator.ps1
@@ -30,4 +30,18 @@ Foreach ($item in $allresults) {
 
     }
 }
-$coll | fl
+$coll | ft
+
+
+$Exchange2010Count = ($Coll | ? {$_.Version -match "14\."} | measure).count
+$Exchange2013Count = ($Coll | ? {$_.Version -match "15\.0"}| measure).count
+$Exchange2016Count = ($coll | ? {$_.Version -match "15\.1"}| measure).count
+
+#NOTE: I am using (Object with filter | measure).count instead of (Object with filter).count directly
+#because when there is only 1 object matching filter, for some reason the (object with filter).count
+#return an empty object ... piping to " measure" which is an alias for Measure-Object, the count
+#seems to be always right, even when there is only 1 object in the Object collection matching the filter.
+
+Write-Host "There are $Exchange2010Count Exchange 2010 servers"
+Write-Host "There are $Exchange2013Count Exchange 2013 servers"
+Write-Host "There are $Exchange2016Count Exchange 2016 servers"
