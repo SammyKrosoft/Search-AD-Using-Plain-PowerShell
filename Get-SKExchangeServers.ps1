4dca63ed75eb8d9bd3315ff52d09f8f0c12da61c
Administrator <Administrator@CANADA.CA>
2020-01-25 22:10:35 -0500

    create function

---
+++ Get-SKExchangeServers.ps1
@@ -0,0 +1,36 @@
+﻿Function Get-SKExchangeServers {
+    $RootADConfigPartition = ([ADSI]'LDAP://RootDSE').configurationNamingContext
+    $ADConnection = [ADSISearcher]""
+    $ADConnection.SearchRoot = "LDAP://$RootADConfigPartition"
+    $ADConnection.Filter = "(objectCategory=CN=MS-Exch-Exchange-Server,CN=Schema,$RootADConfigPartition)"
+    $allresults = $ADConnection.FindAll()
+
+    $Coll = @()
+    Foreach ($item in $allresults) {
+        $coll+=[pscustomobject]@{
+            Name = $Item.Properties['name']
+            #ObjectCategory = $item.Properties['objectcategory']
+            #ObjectClass = $item.Properties['objectClass']
+            #Version1 = $item.Properties['msExchVersion']
+            #VErsion2 = $item.Properties['msExchMinAdminVersion']
+            Version = $Item.Properties['serialNumber']
+            Site = @("$($Item.Properties['msExchServerSite'] -Replace '^CN=|,.*$')")
+            RolesNb = $Item.Properties['msExchCurrentServerRoles']
+            RolesString = Switch ($Item.Properties['msExchCurrentServerRoles']){
+                            2   {"MBX"}
+                            4   {"CAS"}
+                            16  {"UM"}
+                            20  {"CAS, UM" -split ","}
+                            32  {"HUB"}
+                            36  {"CAS, HUB" -split ","}
+                            38  {"CAS, HUB, MBX" -split ","}
+                            54  {"MBX"}
+                            64  {"Edge"}
+                            16385   {"CAS"}
+                            16439   {"CAS, HUB, MBX"  -split ","}
+                                }
+        }
+    }
+Return $Coll
+
+}
